4759c80769dab44b2ee41cabf3eba1eee2f4bbd7
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-02-21 21:38:48 +0200

    add first file
    Primeira ficha de exercicios, flash de Introducao a informatica

---
+++ Aula_1/Ficha_Exercicios.pas
@@ -0,0 +1,54 @@
+Program Pzim ;
+
+var
+	controladorCase: integer;
+	controladorIfAleneas: char;
+	
+	
+	//Numero_1_var
+	//Numero_1_var
+	//Numero_1_var
+	
+
+Begin
+  //Limpar Tela "Console"
+  clrscr;
+  //Mudando cor de fundo
+  textcolor(green);
+  //Menu console 
+  writeln('-------------------------------------------------');
+  writeln('|          Resolucao Da Ficha Exercicios 1       |');
+  writeln('|        Ecolha O Numero Que Deseja Acessar      |');
+  writeln('-------------------------------------------------');
+  writeln;
+  writeln;
+  textcolor(3);
+  writeln('<Press>[1] - Exercicio 1');
+  writeln('<Press>[2] - Exercicio 2');
+  writeln('<Press>[3] - Exercicio 3');
+  writeln('<Press>[4] - Exercicio 4 e alineas');
+  writeln('<Press>[5] - Exercicio 5');
+  
+  read(controladorCase);
+  case controladorCase of
+  
+  	1:	begin
+  	
+  			end;
+  			
+  	2:  begin
+  	
+  			end;
+  			
+  	3:  begin
+  	
+  			end;
+  			
+  	4: 	begin
+  		
+  			end;
+  			
+  
+  end;
+  
+End.
